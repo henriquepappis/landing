109e233e16d6d50aa5d265bf04e43ed76ad6c0f9
Henrique Pappis <falecom@henriquepappis.com>
2017-08-22 20:46:25 -0300

    Ajustes no envio para as redes sociais

--- js/main.js
+++ js/main.js
@@ -1,4 +1,25 @@
-function salvar(){
+function salvar(social = null){	
+	switch (social){
+		case 'facebook':
+			var a = $("<a>").attr("href", "http://henriquepappis.com/cirio/saves/");
+			alert('vai gerar face');
+			break;
+		case 'twitter':
+			$("meta[name='twitter\\:image']").attr("content", "http://henriquepappis.com/cirio/img/youtube.png");
+			var a = $("<a>").attr("href", 
+				"https://twitter.com/intent/tweet?text=Acabei%20de%20criar%20minha%20'Placa do Cirio'%2E%20Crie%20a%20sua%20também%2E%20http://henriquepappis.com/cirio/").appendTo("body");
+			
+			break;
+		case 'download':
+			var a = $("<a>").attr("href", response).attr("download", "placaCirio.png").appendTo("body");
+			alert('vai gerar download');
+			break;
+		default:
+			var a = $("<a>").attr("href", response).attr("download", "placaCirio.png").appendTo("body");
+			alert('vai gerar download default');
+			break;
+	}
+
 	html2canvas($('.image-textarea'), {
 		onrendered: function (canvas) {
 			var imagedata = canvas.toDataURL('image/png');
@@ -11,14 +32,7 @@ function salvar(){
 				},
 				type: 'post',
 				success: function (response) {
-					// $(location).attr({target: '_blank', href: response});
-					var a = $("<a>")
-    					.attr("href", response)
-    					.attr("download", "placaCirio.png")
-    					.appendTo("body");
-
 					a[0].click();
-
 					a.remove();
 				}
 			});
