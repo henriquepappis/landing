505acd08b87037a29f6200d6a24122faf1cc4085
Henrique Pappis <henrique.pappis@simers.org.br>
2017-08-23 09:29:42 -0300

    Ajuste nas ações dos botões das redes sociais

--- js/main.js
+++ js/main.js
@@ -4,38 +4,55 @@ function salvar(social = null){
 			var a = $("<a>").attr("href", "http://henriquepappis.com/cirio/saves/");
 			alert('vai gerar face');
 			break;
+
 		case 'twitter':
 			$("meta[name='twitter\\:image']").attr("content", "http://henriquepappis.com/cirio/img/youtube.png");
-			var a = $("<a>").attr("href", 
-				"https://twitter.com/intent/tweet?text=Acabei%20de%20criar%20minha%20'Placa do Cirio'%2E%20Crie%20a%20sua%20também%2E%20http://henriquepappis.com/cirio/").appendTo("body");
-			
-			break;
-		case 'download':
-			var a = $("<a>").attr("href", response).attr("download", "placaCirio.png").appendTo("body");
-			alert('vai gerar download');
-			break;
-		default:
-			var a = $("<a>").attr("href", response).attr("download", "placaCirio.png").appendTo("body");
-			alert('vai gerar download default');
-			break;
-	}
+			$("meta[property='og\\:image']").attr("content", "http://henriquepappis.com/cirio/img/youtube.png");
+			html2canvas($('.image-textarea'), {
+				onrendered: function (canvas) {
+					var imagedata = canvas.toDataURL('image/png');
+					var imgdata = imagedata.replace(/^data:image\/(png|jpg);base64,/, "");
 
-	html2canvas($('.image-textarea'), {
-		onrendered: function (canvas) {
-			var imagedata = canvas.toDataURL('image/png');
-			var imgdata = imagedata.replace(/^data:image\/(png|jpg);base64,/, "");
-			//ajax call to save image inside folder
-			$.ajax({
-				url: 'salvarImagem.php',
-				data: {
-					imgdata:imgdata
-				},
-				type: 'post',
-				success: function (response) {
-					a[0].click();
-					a.remove();
+					$.ajax({
+						url: 'salvarImagem.php',
+						data: {
+							imgdata:imgdata
+						},
+						type: 'post',
+						success: function (response) {
+							var a = $("<a>").attr("href","https://twitter.com/intent/tweet?text=Acabei%20de%20criar%20minha%20'Placa%20do%20Cirio'%2E%20Crie%20a%20sua%20também%2E%20http://henriquepappis.com/cirio/").appendTo("body");
+							a[0].click();
+							a.remove();
+						}
+					});
 				}
 			});
-		}
-	})
+			break;
+
+		case 'download':
+			html2canvas($('.image-textarea'), {
+				onrendered: function (canvas) {
+					var imagedata = canvas.toDataURL('image/png');
+					var imgdata = imagedata.replace(/^data:image\/(png|jpg);base64,/, "");
+
+					$.ajax({
+						url: 'salvarImagem.php',
+						data: {
+							imgdata:imgdata
+						},
+						type: 'post',
+						success: function (response) {
+							var a = $("<a>").attr("href", response).attr("download", "placaCirio.png").appendTo("body");
+							a[0].click();
+							a.remove();
+						}
+					});
+				}
+			});
+			break;
+
+		default:
+			alert('Erro na solicitação.');
+			break;
+	}
 }
